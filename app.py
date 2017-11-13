d8dd92d342fc5c05f730dbbfdf125306289e6957
Tina <tchen9@stuy.edu>
2017-11-12 23:30:04 -0500

    updated

--- app.py
+++ app.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=NBx9UYnutMTyDhg7i4hc8V6S2yU0wcDaLVwUZZBo")
-    d = json.loads(u.read())
-    return render_template('base.html', title = d['title'], image = d['url'], explanation = d['explanation'])
+    '''u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=NBx9UYnutMTyDhg7i4hc8V6S2yU0wcDaLVwUZZBo")
+    dic = json.loads(u.read())
+    title = dic['title']
+    image = dic['url']
+    explanation = dic['explanation']'''
+
+    ch = urllib2.urlopen("https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.labs.whatWouldMicahSay&access_token=fbe59e316c70d8ade63bd373dc268721&format=json")
+    dic = json.loads(ch.read())
+    micah = dic['micah']
+    stat = dic['stat']
+    
+    #return render_template('base.html', title = title, image = image, explanation=explanation)
+    return render_template('base.html', micah = micah, stat = stat)
+
+
 
 if __name__ == '__main__':
     app.debug = True
